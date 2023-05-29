cb59e6d15a5a6a27b1696e9fda600eb72906073e
OutdatedGuy <everythingoutdated@gmail.com>
2023-05-29 15:44:09 +0530

    chore: updated example app for Flutter 3.10

--- example/macos/Flutter/GeneratedPluginRegistrant.swift
+++ example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,8 @@
 import FlutterMacOS
 import Foundation
 
+import connectivity_plus
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  ConnectivityPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlugin"))
 }
