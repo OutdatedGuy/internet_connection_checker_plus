25fd8b0b270fce5a0e7542c23ad8ee5d864767b6
OutdatedGuy <everythingoutdated@gmail.com>
2023-05-29 15:54:34 +0530

    chore: updated example app for Flutter 3.10 (#5)

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,6 +1,10 @@
+// Dart Packages
 import 'dart:async';
 
+// Flutter Packages
 import 'package:flutter/material.dart';
+
+// This Package
 import 'package:internet_connection_checker_plus/internet_connection_checker_plus.dart';
 
 void main() {
@@ -8,22 +12,23 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Internet Connection Checker Plus Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Internet Connection Checker Plus Demo'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
+  const MyHomePage({super.key, required this.title});
 
   final String title;
 
@@ -55,6 +60,7 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
       body: Center(
