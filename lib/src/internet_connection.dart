364ac4f5c80fc295c3540884263ed245a887425f
OutdatedGuy <everythingoutdated@gmail.com>
2024-01-11 19:53:05 +0530

    refactor: simplified assert condition

--- lib/src/internet_connection.dart
+++ lib/src/internet_connection.dart
@@ -75,10 +75,9 @@ class InternetConnection {
     List<InternetCheckOption>? customCheckOptions,
     bool useDefaultOptions = true,
   }) : assert(
-          useDefaultOptions ||
-              (customCheckOptions != null && customCheckOptions.isNotEmpty),
+          useDefaultOptions || customCheckOptions?.isNotEmpty == true,
           'You must provide a list of options if you are not using the '
-          'default ones',
+          'default ones.',
         ) {
     _internetCheckOptions = [
       if (useDefaultOptions) ..._defaultCheckOptions,
